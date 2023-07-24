a520b0b6c42cd8fd1a1ac432443d0c4402ec1fa7
fernandojerez <fernandojerezy@gmail.com>
2023-07-24 16:50:11 -0400

    parte la base pero tengo un loop que debo solucionar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
-# app/controllers/applications_controller.rb
 class ApplicationController < ActionController::Base
     before_action :authenticate_admin!
-    
-  end
   
+    # Opcional: Define una acción para verificar la autenticación del administrador.
+    def authenticate_admin!
+      unless admin_signed_in?
+        redirect_to new_admin_session_path, alert: 'Acceso denegado. Debes iniciar sesión como administrador.'
+      end
+    end
+  end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   # Con el skip_before_action, evitamos que se aplique el filtro `authenticate_user!` a la acción `index`.
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_admin!, only: [:index]
 
   # La acción `index` del controlador `HomeController` es responsable de mostrar la página de inicio (home).
   def index
